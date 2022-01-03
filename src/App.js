deefbe0f0232884d9c70541d9b7e9a610ca5c132
Guilherme Senci <guilherme.g.senci@gmail.com>
2022-01-03 19:28:23 -0300

    simple reactdom6 use

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route, Link, Outlet } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
+import Home from './Pages/home'
+import Login from './Pages/Login'
+import NoMatch from './Pages/NoMatch';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Routes example</h1>
+
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+      
     </div>
   );
 }
 
-export default App;
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">login</Link>
+          </li>
+          <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+      <Outlet />
+    </div>
+  );
+}
